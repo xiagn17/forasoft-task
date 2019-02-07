f28d89de27a492d1524c7e19e092f0ff76af018d
xiagn17 <xiagn17@gmail.com>
2019-02-08 01:01:36 +0300

    production

---
+++ client/build/precache-manifest.7ac57bb1444c3ac5bb68d519c6305484.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "5884e9687faf57a99fa5",
+    "url": "/static/js/main.5884e968.chunk.js"
+  },
+  {
+    "revision": "bb20bb7cedd7df250041",
+    "url": "/static/js/1.bb20bb7c.chunk.js"
+  },
+  {
+    "revision": "5884e9687faf57a99fa5",
+    "url": "/static/css/main.6656b35d.chunk.css"
+  },
+  {
+    "revision": "a7c655cd56a42c5f9a1d2315edb706fd",
+    "url": "/index.html"
+  }
+];
